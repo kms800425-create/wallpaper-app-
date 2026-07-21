53a178a1c6609f6a25e2779389845d40d2316d52
kms800425-create <kms800425@gmail.com>
2026-07-21 11:00:23 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,57 @@
-const CACHE_NAME = 'barun-suyu-app-v1';
+const CACHE_NAME = 'barun-suyu-app-v2';
+
 const urlsToCache = [
+
   './',
+
   './index.html',
+
   './manifest.json'
+
 ];
 
 self.addEventListener('install', event => {
+
+  self.skipWaiting();
+
   event.waitUntil(
+
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+
   );
+
+});
+
+self.addEventListener('activate', event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys =>
+
+      Promise.all(
+
+        keys
+
+          .filter(key => key !== CACHE_NAME)
+
+          .map(key => caches.delete(key))
+
+      )
+
+    )
+
+  );
+
+  self.clients.claim();
+
 });
 
 self.addEventListener('fetch', event => {
+
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+
+    fetch(event.request).catch(() => caches.match(event.request))
+
   );
+
 });
